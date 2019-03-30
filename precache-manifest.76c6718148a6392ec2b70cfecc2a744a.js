d5c1a539f94a6119ed73ce2fc4f57a2b05d6868a
rlisboars <rlisboars@gmail.com>
2019-03-30 18:26:21 -0300

    Updates

---
+++ precache-manifest.76c6718148a6392ec2b70cfecc2a744a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dad4d8b9fb180c6c84f",
+    "url": "/react-2048/static/js/main.1a2f90cf.chunk.js"
+  },
+  {
+    "revision": "771102ff54dc5576a729",
+    "url": "/react-2048/static/js/runtime~main.f391b90c.js"
+  },
+  {
+    "revision": "4e927a93d1b6982c5e88",
+    "url": "/react-2048/static/js/2.0c532849.chunk.js"
+  },
+  {
+    "revision": "23714b912089300843dbeb03a622eb2a",
+    "url": "/react-2048/index.html"
+  }
+];
